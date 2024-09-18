7babbb7ebd6593ba61da4f337807c2010eec478b
derickferreira <derickferreira036@gmail.com>
2024-09-18 19:24:41 +0100

    I'm back, just looking over it

--- threejs/three01/src/js/script.js
+++ threejs/three01/src/js/script.js
@@ -1,119 +1,23 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from "dat.gui";
 
-const rendered = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 
-rendered.shadowMap.enabled = true;
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-rendered.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(rendered.domElement);
+document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  45,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-const orbit = new OrbitControls(camera, rendered.domElement);
-
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.set(-10, 30, 30);
-orbit.update();
+camera.position.set(5, 5, 10);
 
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const box = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(box);
-
-const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  side: THREE.DoubleSide,
-});
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-scene.add(plane);
-plane.rotation.x = -Math.PI / 2;
-plane.receiveShadow = true;
-
-const gridHelper = new THREE.GridHelper(30);
-scene.add(gridHelper);
-
-const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  wireframe: false,
-});
-// const sphereMaterial = new THREE.MeshBasicMaterial({
-//   color: 0xff00ff,
-//   wireframe: true,
-// });
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add(sphere);
-sphere.position.set(-10, 10, 0);
-sphere.castShadow = true;
-
-const ambientLight = new THREE.AmbientLight(0x333333);
-scene.add(ambientLight);
-
-/*
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-scene.add(directionalLight);
-directionalLight.position.set(-30, 50, 0);
-directionalLight.castShadow = true;
-directionalLight.shadow.camera.bottom = -10;
-
-const dLightHelper = new THREE.DirectionalLightHelper(directionalLight);
-scene.add(dLightHelper);
-
-const dLightShadowHelper = new THREE.CameraHelper(
-  directionalLight.shadow.camera
-);
-scene.add(dLightShadowHelper);
-*/
-
-const spotLight = new THREE.SpotLight(0xffffff);
-scene.add(spotLight);
-spotLight.position.set(-100, 100, 0);
-spotLight.castShadow = true;
-
-const sLightHelper = new THREE.SpotLightHelper(spotLight)
-scene.add(sLightHelper);
-
-const gui = new dat.GUI();
-
-const options = {
-  sphereColor: "#ffea00",
-  wireframe: false,
-  speed: 0.01,
-};
-
-gui.addColor(options, "sphereColor").onChange((e) => {
-  sphere.material.color.set(e);
-});
-
-gui.add(options, "wireframe").onChange((e) => {
-  sphere.material.wireframe = e;
-});
-
-gui.add(options, "speed", 0, 0.1);
-
-let step = 0;
-
-const animation = () => {
-  box.rotation.x += 0.05;
-  box.rotation.y += 0.05;
-
-  step += options.speed;
-  sphere.position.y = 10 * Math.abs(Math.sin(step));
-
-  rendered.render(scene, camera);
-};
-
-rendered.setAnimationLoop(animation);
+renderer.render(scene, camera);
