9517ac866b09746fcd5ff3674f9f203bb8449c98
derickferreira <derickferreira036@gmail.com>
2024-09-20 07:51:58 +0100

    knex settings done. I did some scripts in package.json to run the codes in a easy way too.

---
+++ api-rest-ts-node/src/server/database/migrations/0000_create_city.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+export async function up(knex: Knex) {
+    return knex.schema
+        .createTable(ETableNames.city, (table) => {
+            table.bigIncrements("id").primary().index();
+            table.string("name", 150).index().notNullable();
+            table.comment("This table is used to store cities in the system.");
+        })
+        .then(() => {
+            console.log(`# Created table ${ETableNames.city}`);
+        });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable(ETableNames.city).then(() => {
+        console.log(`# Down table ${ETableNames.city}`);
+    });
+}
