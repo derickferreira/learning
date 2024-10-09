82a1a57cc0ff934051df38954f59cfe69a6ba30e
derickferreira <derickferreira036@gmail.com>
2024-10-09 07:08:17 +0100

    features: Seeds had been added and also in the server/index.ts I added a script that will run the seed automatically.

--- api-rest-ts-node/src/index.ts
+++ api-rest-ts-node/src/index.ts
@@ -1,5 +1,24 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";
 
-server.listen(process.env.PORT || 3333, () =>
-    console.log(`app running in the port: ${process.env.PORT}`)
-);
+const startServer = () =>
+    server.listen(process.env.PORT || 3333, () =>
+        console.log(`app running in the port: ${process.env.PORT}`)
+    );
+
+if (process.env.IS_LOCALHOST !== "true") {
+    console.log("Imigration is running and seed");
+
+    Knex.migrate
+        .latest()
+        .then(() => {
+            Knex.seed
+                .run()
+                .then(() => startServer())
+                .catch(console.log);
+        })
+        .catch(console.log);
+} else {
+    startServer();
+    console.log("Server is running on localhost");
+}
