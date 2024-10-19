109e2eba73c07d93abd9c400a9706465e6e3b9d2
derickferreira <derickferreira036@gmail.com>
2024-10-19 07:43:10 +0100

    Feat: users table added

--- api-rest-ts-node/src/server/database/ETableNames.ts
+++ api-rest-ts-node/src/server/database/ETableNames.ts
@@ -1,4 +1,5 @@
 export enum ETableNames {
     city = "city",
     people = "people",
+    user = "user",
 }
---
+++ api-rest-ts-node/src/server/database/migrations/0002_create_users.ts
@@ -0,0 +1,35 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+export async function up(knex: Knex) {
+    return knex.schema
+        .createTable(ETableNames.user, (table) => {
+            table.bigIncrements("id").primary().index();
+
+            table
+                .string("name", 30)
+                .checkLength("<=", 30)
+                .notNullable()
+                .checkLength(">", 3);
+
+            table
+                .string("email", 100)
+                .checkLength("<=", 100)
+                .index()
+                .unique()
+                .notNullable();
+
+            table.string("password", 100).checkLength(">", 5);
+
+            table.comment("This table is used to store users in the system.");
+        })
+        .then(() => {
+            console.log(`# Created table ${ETableNames.user}`);
+        });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable(ETableNames.user).then(() => {
+        console.log(`# Down table ${ETableNames.user}`);
+    });
+}
